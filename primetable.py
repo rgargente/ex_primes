06c15a8b9b659d32f1968eecca66c0570d2520d1
rgargente <rgargente@gmail.com>
2018-09-13 22:23:36 +0100

    conda environment file

--- primetable.py
+++ primetable.py
@@ -1,12 +1,13 @@
-from tabulate import tabulate
-import primes
 import numpy as np
+from tabulate import tabulate
+
+from primes import get_primes_up_to_nth_smart
 
 if __name__ == "__main__":
-    primes10 = primes.get_primes_up_to_nth_smart(10)
-    size = len(primes10)
-    rows = np.tile(primes10, (size, 1))
-    for i, p in enumerate(primes10):
+    primes = get_primes_up_to_nth_smart(10)
+    size = len(primes)
+    rows = np.tile(primes, (size, 1))
+    for i, p in enumerate(primes):
         rows[i, :] *= p
-    rows = np.c_[primes10, rows]
-    print(tabulate(rows, headers=[''] + primes10, tablefmt='grid'))
+    rows = np.c_[primes, rows]
+    print(tabulate(rows, headers=[''] + primes, tablefmt='grid'))
