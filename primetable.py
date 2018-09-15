67fca784c004ffadf92cc02af647e94f570f5d46
rgargente <rgargente@gmail.com>
2018-09-15 12:59:40 +0100

    table optimization

--- primetable.py
+++ primetable.py
@@ -6,8 +6,11 @@ from primes import get_primes_up_to_nth_smart
 if __name__ == "__main__":
     primes = get_primes_up_to_nth_smart(10)
     size = len(primes)
-    rows = np.tile(primes, (size, 1))
-    for i, p in enumerate(primes):
-        rows[i, :] *= p
+    rows = np.zeros((size, size))
+    for i in range(size): # This loop avoids calculating twice the same value (the table is symmetrical )
+        for j in range(i, size):
+            p = primes[i] * primes[j]
+            rows[i, j] = p
+            rows[j, i] = p
     rows = np.c_[primes, rows]
     print(tabulate(rows, headers=[''] + primes, tablefmt='grid'))
